e8320e973f3cd57c07419c4196d5344765e46d03
Nikita Sonawane <nrsonawane27@gmail.com>
2024-12-24 14:00:06 +0000

    operator

---
+++ Basics/Operator.js
@@ -0,0 +1,33 @@
+console.log("My name is nikita!!!");
+
+// template literals
+let a = 5;
+let b = 10;
+console.log("The addition is : " ,a+b , ".");
+console.log(`The addition is : ${a+b} .`);
+
+console.log("\nArithmetic  operator");
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+console.log("\nUnary  operator");
+console.log(a++);
+console.log(++a)
+console.log(a--);
+console.log(--a);
+
+console.log("\nComparison  operator");
+console.log(a<b);
+console.log(a<=b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a==b);
+console.log(a!=b);
+
+
+
+
