32fb7bc215222bd5efbb9d3af0c60e1f3b9abc01
Nikita Sonawane <nrsonawane27@gmail.com>
2024-12-25 17:12:51 +0000

    if

---
+++ ControlStatement/if.js
@@ -0,0 +1,26 @@
+let color = "yelow";
+if(color === "red") {
+    console.log(`The color of traffic light is ${color}`);
+} else if(color === "yellow") {
+    console.log(`The color of traffic light is ${color}`);
+} else if(color === "green") {
+    console.log(`The color of traffic light is ${color}`);
+} else {
+    console.log("Traffic light is broken");
+}
+
+console.log("\n Calculate popcorn price:");
+let size = "XL";
+if(size === "XL") {
+    console.log("The price of popcron is Rs.250");
+} else if(size === "L") {
+    console.log("The price of popcron is Rs.200");
+} else if(size === "M") {
+    console.log("The price of popcron is Rs.100");
+} else if(size === "S") {
+    console.log("The price of popcron is Rs.50");
+} else {
+    console.log("Invalid size");
+}
+
+
